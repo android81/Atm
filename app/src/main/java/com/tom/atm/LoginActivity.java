b43ee9a342e0c61aec3f01dddd7f0b2c15f3323e
Hank Tom <hanktom@gmail.com>
2018-01-22 22:25:21 +0800

    記住登入帳號實作

--- app/src/main/java/com/tom/atm/LoginActivity.java
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.tom.atm;
 
 import android.app.AlertDialog;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText edUserid = findViewById(R.id.ed_userid);
+        SharedPreferences setting =
+                getSharedPreferences("atm", MODE_PRIVATE);
+        edUserid.setText(setting.getString("PREF_USERID", ""));
     }
 
     public void login(View view){
@@ -21,6 +26,11 @@ public class LoginActivity extends AppCompatActivity {
         String uid = edUserid.getText().toString();
         String pw = edPasswd.getText().toString();
         if (uid.equals("jack") && pw.equals("1234")){ //登入成功
+            SharedPreferences setting =
+                    getSharedPreferences("atm", MODE_PRIVATE);
+            setting.edit()
+                    .putString("PREF_USERID", uid)
+                    .apply();
             Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
             getIntent().putExtra("LOGIN_USERID", uid);
             getIntent().putExtra("LOGIN_PASSWD", pw);
