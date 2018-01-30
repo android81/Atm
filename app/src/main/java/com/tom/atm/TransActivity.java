97337726e19f3d8176df038409c86960801c786c
Hank Tom <hanktom@gmail.com>
2018-01-30 20:17:50 +0800

    使用OkHttp第三方函式庫

--- app/src/main/java/com/tom/atm/TransActivity.java
+++ app/src/main/java/com/tom/atm/TransActivity.java
@@ -4,19 +4,45 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class TransActivity extends AppCompatActivity {
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trans);
-        new TransTask().execute("http://atm201605.appspot.com/h");
+        Request request = new Request.Builder()
+                .url("http://atm201605.appspot.com/h")
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                Log.d("OKHTTP", json);
+                //解析JSON
+                parseJSON(json);
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //告知使用者連線失敗
+            }
+        });
+//        new TransTask().execute("http://atm201605.appspot.com/h");
     }
 
     class TransTask extends AsyncTask<String, Void, String> {
@@ -28,7 +54,7 @@ public class TransActivity extends AppCompatActivity {
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(url.openStream()));
                 String line = in.readLine();
-                while(line != null){
+                while (line != null) {
                     Log.d("HTTP", line);
                     sb.append(line);
                     line = in.readLine();
